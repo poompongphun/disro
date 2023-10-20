8eb26fbc95b3efa4c0bd2143a9d66f1db249afd1
Phe-nix <88503925+Phe-nix@users.noreply.github.com>
2023-10-21 01:21:36 +0700

    :lipstick: : built page setting ui

---
+++ app/setting/page.tsx
@@ -0,0 +1,10 @@
+const setting = () => {
+    return (
+      <div className="flex justify-center items-center h-full">
+        <h1>Hello World</h1>
+      </div>
+    );
+  };
+  
+  export default setting;
+  
