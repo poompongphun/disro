ca7aaebb56420ca56e59212e4f228a96dedef04d
poompongphun <poompongphun@gmail.com>
2023-11-10 00:24:22 +0700

    websocket chat

--- app/projects/[id]/[room]/page.tsx
+++ app/projects/[id]/[room]/page.tsx
@@ -5,23 +5,43 @@ import mockData from "@/Class/mockData.json";
 import Room from "@/Class/Room";
 import ChatRoom from "@/components/Project/Room/ChatRoom";
 import BoardRoom from "@/components/Project/Room/BoardRoom";
+import { io } from "socket.io-client";
+import { useEffect, useState } from "react";
 
 const Room = () => {
   // MockData
   const { id, room: roomParam } = useParams();
   const project: Project = mockData.find(
-    (project) => project.id === Number(id)
+    (project) => project.id === String(id)
   ) as Project;
   const AllRoom: Room[] = project.categories
     .map((category) => category.rooms)
     .flat();
   const currentRoom: Room = AllRoom.find(
-    (room) => room.id === Number(roomParam)
+    (room) => room.id === String(roomParam)
   ) as Room;
   console.log(currentRoom);
 
+  const [showChat, setShowChat] = useState(false);
+
+  var socket: any;
+  socket = io("http://localhost:3001");
+
+  const handleJoin = () => {
+    if (currentRoom.id !== "") {
+      socket.emit("join_room", currentRoom.id);
+      setShowChat(true);
+    } else {
+      alert("Room Id is not valid");
+    }
+  };
+
+  useEffect(() => {
+    handleJoin();
+  }, []);
+
   return currentRoom.type === "text" ? (
-    <ChatRoom room={currentRoom}></ChatRoom>
+    <ChatRoom room={currentRoom} socket={socket}></ChatRoom>
   ) : (
     <BoardRoom room={currentRoom}></BoardRoom>
   );
--- components/Project/Room/ChatRoom.tsx
+++ components/Project/Room/ChatRoom.tsx
@@ -1,9 +1,67 @@
+"use client";
 import Room from "@/Class/Room";
 import HashtagIcon from "@heroicons/react/20/solid/esm/HashtagIcon";
 import ArrowUpTrayIcon from "@heroicons/react/20/solid/esm/ArrowUpTrayIcon";
 import ChatMessage from "@/components/Project/Room/ChatMessage";
+import { useEffect, useState } from "react";
 
-const ChatRoom = ({ room }: { room: Room }) => {
+const messagesDB = [
+  {
+    user: {
+      id: "1",
+      username: "Member 1",
+      image:
+        "https://yt3.googleusercontent.com/-CFTJHU7fEWb7BYEb6Jh9gm1EpetvVGQqtof0Rbh-VQRIznYYKJxCaqv_9HeBcmJmIsp2vOO9JU=s900-c-k-c0x00ffffff-no-rj",
+    },
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusda",
+  },
+];
+
+interface Message {
+  roomId?: string;
+  user: {
+    id: string;
+    username: string;
+    image: string;
+  };
+  text: string;
+}
+
+const ChatRoom = ({ room, socket }: { room: Room; socket: any }) => {
+  const [currentMsg, setCurrentMsg] = useState("");
+  const [messages, setMessages] = useState<Message[]>(messagesDB);
+
+  const sendData = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (currentMsg !== "") {
+      const msgData: Message = {
+        roomId: room.id,
+        user: {
+          id: "1",
+          username: "Member 1",
+          image:
+            "https://yt3.googleusercontent.com/-CFTJHU7fEWb7BYEb6Jh9gm1EpetvVGQqtof0Rbh-VQRIznYYKJxCaqv_9HeBcmJmIsp2vOO9JU=s900-c-k-c0x00ffffff-no-rj",
+        },
+        text: currentMsg,
+      };
+      await socket.emit("send_msg", msgData);
+      setCurrentMsg("");
+    }
+  };
+
+  useEffect(() => {
+    console.log("receive_msg");
+    const func = (data: Message) => {
+      console.log("receive_msg work");
+      setMessages((pre) => [data, ...pre]);
+    };
+
+    socket.on("receive_msg", func);
+    return () => {
+      socket.off("receive_msg", func);
+    };
+  }, []);
+
   return (
     <div className="h-full flex justify-between flex-col">
       <div>
@@ -17,16 +75,11 @@ const ChatRoom = ({ room }: { room: Room }) => {
         className="flex flex-col-reverse gap-2 p-2 w-full overflow-y-auto no-scrollbar"
         style={{ height: "calc(100% - 108px)" }}
       >
-        {"11111111111111".split("").map((_, index) => (
+        {messages.map((message, index) => (
           <ChatMessage
             key={index}
-            user={{
-              id: 1,
-              username: "Member 1",
-              image:
-                "https://yt3.googleusercontent.com/-CFTJHU7fEWb7BYEb6Jh9gm1EpetvVGQqtof0Rbh-VQRIznYYKJxCaqv_9HeBcmJmIsp2vOO9JU=s900-c-k-c0x00ffffff-no-rj",
-            }}
-            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdamssssssssssssssssssssssssssssssssssssssss."
+            user={message.user}
+            text={message.text}
           ></ChatMessage>
         ))}
         <div className="w-full">
@@ -37,15 +90,17 @@ const ChatRoom = ({ room }: { room: Room }) => {
           <hr className="my-4" />
         </div>
       </div>
-      <div className="p-2 relative">
+      <form className="p-2 relative" onSubmit={(e) => sendData(e)}>
         <button className="absolute top-5 left-6">
           <ArrowUpTrayIcon className="w-6 h-6"></ArrowUpTrayIcon>
         </button>
         <input
           className="h-12 pl-12 pr-4 rounded-full border-[1.5px] border-white bg-transparent w-full placeholder-slate-300 outline-none "
           placeholder={`Message  #${room.name}`}
+          value={currentMsg}
+          onChange={(e) => setCurrentMsg(e.target.value)}
         ></input>
-      </div>
+      </form>
     </div>
   );
 };
