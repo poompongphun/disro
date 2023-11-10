0544e93dc38d5b4ff8224a4c3f2f9a4292cc8e62
poompongphun <poompongphun@gmail.com>
2023-11-10 18:02:55 +0700

    fix err

--- components/Layouts/MainLayout.tsx
+++ components/Layouts/MainLayout.tsx
@@ -6,7 +6,7 @@ import HomeLayout from "./HomeLayout";
 import SettingLayout from "./SettingLayout";
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
-  const isLayoutNeeded = [`/login`, `/register`].includes(pathname);
+  const isLayoutNeeded = [`/login`, `/register`].includes(String(pathname));
   const LayoutComponent = isLayoutNeeded ? AuthLayout : HomeLayout;
   if (pathname === `/setting`) {
     return <SettingLayout>{children}</SettingLayout>;
